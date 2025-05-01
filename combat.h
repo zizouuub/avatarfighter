6dff0539be95b05fda788d72227c7cb6a2bf2cb3
linaprns <linaprrns@gmail.com>
2025-05-01 17:06:56 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -2,6 +2,39 @@
 #include <stdio.h> 
 #include "effets.h"
 #include <string.h>
-#define MAX_TECHNIQUES 3
 #define MAX_NOM 50 //taille du nom
 #define MAX_DESC 150 //Description de la technique
+
+
+typedef enum {
+    FEU, EAU, TERRE, AIR
+} Element;
+
+// Effets possibles des techniques spéciales
+typedef enum {
+    ATTAQUE, DEFENSE, PV, AGILITE, VITESSE, STUN, SOIN, BRULURE, CONTRE, SILENCE
+} EffetType;
+
+
+
+typedef struct {
+    char nom[MAX_NOM];
+    char description[MAX_DESC];
+    EffetType effet;     // texte dans le fichier ex : "ATTAQUE" "DEFENSE"
+    int valeur;
+    int duree;           // en tours
+    int recharge;
+    int tours_restant;   // à initialiser à 0 à la lecture
+} TechniqueSpeciale;
+
+
+typedef struct {
+    char nom[MAX_NOM];
+    Element element;
+    int pv_max;
+    int attaque;
+    int defense;
+    int agilite;
+    int vitesse;
+    TechniqueSpeciale techniques[3];  //fixe à 3 techniques
+} Combattant;
