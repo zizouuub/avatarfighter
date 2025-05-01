b5af35258dcbee2ed1cc6b8245ddbe70a39ad74c
linaprns <linaprrns@gmail.com>
2025-05-01 16:58:26 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,3 +1,7 @@
 #include "types.h" 
 #include <stdio.h> 
 #include "effets.h"
+#include <string.h>
+#define MAX_TECHNIQUES 3
+#define MAX_NOM 50 //taille du nom
+#define MAX_DESC 150 //Description de la technique
