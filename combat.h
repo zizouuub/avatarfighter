79ebe26bb7f67e6e5765217d6fa553506dd8f419
zizouuub <zboutlidjane@gmail.com>
2025-05-08 16:25:37 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,39 +1,4 @@
 #include "types.h" 
 #include <stdio.h> 
 #include "effets.h"
-#include <string.h>
-#define MAX_NOM 50 
-#define MAX_DESC 150 
 
-
-typedef enum {
-    FEU, EAU, TERRE, AIR
-} Element;
-
-typedef enum {
-    ATTAQUE, DEFENSE, PV, AGILITE, STUN, SOIN, BRULURE, CONTRE, POISON
-} EffetType;
-
-
-
-typedef struct {
-    char nom[MAX_NOM];
-    char description[MAX_DESC];
-    EffetType effet; 
-    int valeur;
-    int duree; // en tours
-    int recharge;
-    int tours_restant; // à initialiser à 0 à la lecture
-} TechniqueSpeciale;
-
-
-typedef struct {
-    char nom[MAX_NOM];
-    Element element;
-    int pv_max;
-    int attaque;
-    int defense;
-    int agilite;
-    int vitesse;
-    TechniqueSpeciale techniques[3]; //fixe à 3 techniques
-} Combattant;
