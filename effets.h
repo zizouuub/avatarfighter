d91ee6df14da6e0c8a3b3bb7a5c69aea9024b9e9
zizouuub <zboutlidjane@gmail.com>
2025-05-08 18:01:35 +0200

    Update effets.h

--- effets.h
+++ effets.h
@@ -7,11 +7,10 @@ void initialiser_combattant(Combattant *c);
 void appliquerEffetElementaire(Combattant *cible, TechniqueSpeciale effet);
 
 // Applique les dégâts ou conséquences d'un effet 
-void appliquerdegats(Combattant *c);
+void appliquerDegats(Combattant *c);
 
 // Met à jour la durée des effets
 void mettreAJourEffets(Combattant *combattant, TechniqueSpeciale *tech);
 
 // Retourne un multiplicateur de dégâts selon l’effet 
-float multiplicateur(EffetType e);
-
+float multiplicateur(Element attaquant, Element defenseur);
