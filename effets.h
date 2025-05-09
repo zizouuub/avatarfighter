a6f17269bca2257e5fe4b28629a3f4eb21bee90c
zizouuub <zboutlidjane@gmail.com>
2025-05-09 20:59:12 +0200

    Update effets.h

--- effets.h
+++ effets.h
@@ -7,7 +7,7 @@ void initialiser_combattant(Combattant *c);
 void appliquerEffetElementaire(Combattant *cible, TechniqueSpeciale effet);
 
 // Applique les dégâts ou conséquences d'un effet 
-void appliquerDegats(Combattant *c, TechniqueSpeciale *tech);
+void appliquerDegats(Combattant *c);
 
 // Met à jour la durée des effets
 void mettreAJourEffets(Combattant *combattant, TechniqueSpeciale *tech);
@@ -18,3 +18,8 @@ float multiplicateur(Element attaquant, Element defenseur);
 // gel un combattant
 int est_incapacite(Combattant *c);
 
+// Fonction pour calculer les dégâts d'une attaque
+int calculerDegats(Combattant *attaquant, TechniqueSpeciale *tech);
+
+// Fonction qui utilise l'agilité pour déterminer si l'esquive réussit
+int tentativeEsquive(Combattant *defenseur);
