300950249b923772c0479c61ffc2998bd9b0e517
zizouuub <zboutlidjane@gmail.com>
2025-05-10 09:48:34 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -5,19 +5,22 @@
 #include "effets.h"
 #include "type.h" 
 
-// Initialise les membres de chaque équipe
+// Fonction pour initialiser le combat
 void initialiser_combat(Combattant *equipe1, int taille1, Combattant *equipe2, int taille2);
 
-// Vérifie si une équipe est complètement KO
+// Fonction pour vérifier si une équipe est KO
 int equipe_est_KO(Combattant *equipe, int taille);
 
-// Attaque normale d’un combattant sur une cible
+// Fonction pour calculer les dégâts d'une attaque normale  
 void attaque_normale(Combattant *attaquant, Combattant *cible);
 
-// Gère le tour d’un combattant contre une équipe adverse
+// Fonction pour utiliser une technique spéciale
+void utiliserTechnique(Combattant *attaquant, Combattant *cible, TechniqueSpeciale *tech);
+
+// Fonction pour choisir une action (attaquer ou utiliser une technique)
 void effectuer_tour(Combattant *joueur, Combattant *adversaires, int taille_adversaires);
 
-// Gère toute la boucle de combat entre deux équipes
+// Fonction pour gérer le combat entre deux équipes
 void boucle_de_combat(Combattant *equipe1, int taille1, Combattant *equipe2, int taille2);
 
 #endif
