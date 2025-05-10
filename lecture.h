00764519bb7a9055b02c62481a9710bd624ec8d1
zizouuub <zboutlidjane@gmail.com>
2025-05-10 22:41:44 +0200

    Update lecture.h

--- lecture.h
+++ lecture.h
@@ -1,7 +1,13 @@
 #ifndef LECTURE_H
 #define LECTURE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "effets.h"
 #include "type.h"
+#define MAX_PERSOS 10
+
 
 // Charge les combattants depuis un fichier
 int charger_combattants(const char *nom_fichier, Combattant *persos, int max_persos);
