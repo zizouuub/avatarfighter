66f2d22c467246ae0a3dafd526bd34619d6cdf01
zizouuub <zboutlidjane@gmail.com>
2025-05-11 14:14:35 +0200

    Create joueurVSordi.h

---
+++ joueurVSordi.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "combat.h"  // Pour attaque_normale
+#include "effets.h"  // Pour appliquerEffetElementaire
+
+int choisir_action_joueur() ; 
+
+int choisir_action_ia(Combattant ia);
+
+// Simule l’utilisation d’une technique spéciale (exemple simplifié)
+void utiliser_technique(Combattant *source, Combattant *cible);
+
+void tour(Combattant *joueur, Combattant *ia, int est_joueur);
+
+void lancer_combat(Combattant *joueur, Combattant *ia);
