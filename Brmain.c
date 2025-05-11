08ddd9fcba4c38f4d263bc1aaa108f8a844f486a
Akani-S <akanisugir@gmail.com>
2025-05-11 22:22:40 +0200

    Update Brmain.c

--- Brmain.c
+++ Brmain.c
@@ -1,6 +1,13 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "interface.h"
 #include "type.h"
+#include "lecture.h"
+#include "effets.h"
+#include "joueurVSordi.h"
+#include "combat.h"
 
 int main(){
     srand(time(NULL));
@@ -11,12 +18,21 @@ int main(){
     Combattant tab_combattants[MAX_PERSOS];
 
     // Chargement des combattants depuis le fichier
-    int nb_combattants = chargerCombattants("fichier.txt", tab_combattants);
+    int nb_combattants = charger_combattants("fichier.txt", tab_combattants, MAX_PERSOS);
+
     if (nb_combattants <= 0) {
         printf(ROUGE "Erreur: Aucun combattant chargé.\n" RESET);
         return 1;
     }
 
+    //déclaration des équipes 
+    Combattant equipe_joueur[3];  // une équipe de 3 combattants JOUEUR
+    Combattant equipe_ordi[3]; //une équipe de 3 combattants ORDI
+
+    Combattant equipe_J1[3]; //une équipe de 3 combattants JOUEUR 1
+    Combattant equipe_J2[3]; //une équipe de 3 combattants JOUEUR 2
+
+    
     // Affichage du menu principal
     int choix = choixPrincipal();  // Premier appel
     if (choix == 1) {
@@ -24,50 +40,64 @@ int main(){
         int mode = choixModeJeu();
 
         // Sélection des combattants
-        printf("Vous allez mainentant choisir vos équipes de" ROUGE" 3 combattants"RESET" !\n");
-        printf("(Attention à descendre pour voir tous les combattants !)\n");
+        printf("Vous allez maintenant choisir vos équipes de" ROUGE" 3 combattants"RESET" !\n");
+        printf("(Attention à bien remonter/descendre pour voir tous les combattants et vos équipes !)\n");
         sleep(4);
-        printf("\033[H\033[2J");
-        
+        clearScreen();
         if (mode == 1) {
-            
+            // Mode joueur contre ordinateur
 
-            // Afficher la liste des combattants une seule fois ici
-            afficheCombattants(tab_combattants, nb_combattants);
-
-            Combattant equipe_joueur[3];  // par exemple, une équipe de 3 combattants
+            //sélection joueur
             selectionnerEquipe(tab_combattants, &nb_combattants, equipe_joueur, 3);
 
             //sélection ordi 
-            Combattant equipe_ordi[3];
             choisirEquipeAleatoire(tab_combattants, nb_combattants, equipe_ordi, 3);
 
+            //effacer écran
+            clearScreen();
+            printf("\n\n\n");
             //affichage équipe joueur
-            printf("\033[H\033[2J");
-            printf("   === " JAUNE"Votre équipe"RESET": ===\n");
+            printf("\n\n      ======== " JAUNE"Votre équipe"RESET": ========\n");
             afficherEquipe(equipe_joueur, 3);
 
             //affichage équipe ordi
-            printf("\n\n");
-            printf("   === " JAUNE"Equipe de l'ordinateur"RESET": ===\n");
+            printf("\n\n      ======= " JAUNE"Equipe de l'ordinateur"RESET": =======\n");
             afficherEquipe(equipe_ordi, 3);
-        } else{
-
+        } else {
             // Mode joueur contre joueur
-            Combattant equipe_J1[3];
-            Combattant equipe_J2[3];
+
+            //sélection des deux équipes
             selectionnerEquipesJoueurs(tab_combattants, &nb_combattants, equipe_J1, equipe_J2, 3);
 
-            //affichage équipe joueur 1
-            printf("\033[H\033[2J");
-            printf("   === " JAUNE"Equipe Joueur 1"RESET": ===\n");
-            afficherEquipe(equipe_J1, 3);
+            //effacer écran
+            clearScreen();
+            printf("\n\n\n");
+            //affichage du joueur 1
+            printf("\n\n      =======" JAUNE"Equipe Joueur 1"RESET": =======\n");
+            afficherEquipe(equipe_J1, 3);  
 
-            //affichage équipe joueur 2
-            printf("\n\n");
-            printf("   === " JAUNE"Equipe Joueur 2"RESET": ===\n");
+            //affichage du joueur 2
+            printf("\n\n      ======= " JAUNE"Equipe Joueur 2"RESET": =======\n"); 
             afficherEquipe(equipe_J2, 3);
+
+            
         }
+
+        //affichage jeu commence
+        jeuCommence();
+
+        //lancement du combat
+        if (mode == 1){
+            lancer_combat(equipe_joueur, 3, equipe_ordi, 3);
+        }else if (mode == 2){
+            boucle_de_combat(equipe_J1, 3, equipe_J2, 3);
+        }
+
+        
     }
+    //quitter le jeux 
+    finJeu();
+    exit(0);
+    
     return 0;
 }
