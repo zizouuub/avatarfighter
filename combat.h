7a84a7ee86c5016b84ec7aede48916a75f95f987
zizouuub <zboutlidjane@gmail.com>
2025-05-11 21:35:03 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -13,9 +13,8 @@ void initialiser_combat(Combattant *equipe1, int taille1, Combattant *equipe2, i
 // Affiche une introduction au combat (et initialise le random)
 void initialiser_comb(Combattant *equipe1, int taille1, Combattant *equipe2, int taille2);
 
-// -------------------------
-// MISE À JOUR DES COMBATTANTS
-// -------------------------
+// Initialise un seul combattant (doit être définie dans un .c ou header externe)
+void initialiser_combattant(Combattant *perso);
 
 // Met à jour les compteurs de tour et de recharge des techniques
 void maj_tours_combattants(Combattant *equipe, int taille);
@@ -23,19 +22,12 @@ void maj_tours_combattants(Combattant *equipe, int taille);
 // Définit quand le personnage pourra rejouer
 void definir_prochain_tour(Combattant *perso);
 
-// -------------------------
-// CHOIX DU JOUEUR
-// -------------------------
 
 // Permet de choisir un combattant actif parmi l'équipe
 int choisir_combattant(Combattant *equipe, int taille);
 
 // Permet de choisir une cible parmi l'équipe adverse
 int choisir_cible(Combattant *equipe, int taille);
-
-// -------------------------
-// ACTIONS EN COMBAT
-// -------------------------
 
 // Effectue une attaque normale (calcul de dégâts, etc.)
 void attaque_normale(Combattant *attaquant, Combattant *cible);
@@ -45,10 +37,6 @@ void utiliserTechnique(Combattant *attaquant, Combattant *cible, TechniqueSpecia
 
 // Réalise un tour pour un combattant (choisit une action)
 void effectuer_tour(Combattant *joueur, Combattant *adversaires, int taille_adversaires);
-
-// -------------------------
-// CONTRÔLE DU COMBAT
-// -------------------------
 
 // Vérifie si toute une équipe est KO
 int equipe_est_KO(Combattant *equipe, int taille);
