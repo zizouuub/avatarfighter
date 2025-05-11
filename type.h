510b7066c6a4346036c5dab09d808d551aa7d926
Akani-S <akanisugir@gmail.com>
2025-05-11 17:31:00 +0200

    Update type.h

--- type.h
+++ type.h
@@ -1,6 +1,15 @@
+#ifndef TYPE_H
+#define TYPE_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 #define MAX_STR 80
 #define MAX_TECHS 3      // Nombre de techniques par personnage
 #define NB_EFFETS 8     // Nombre d'effets possibles
+
+
 
 // Enumération pour les éléments
 typedef enum {
@@ -9,16 +18,16 @@ typedef enum {
 
 // Enumération pour les types d'effets d'une technique
 typedef enum {
-    AUCUN, ATTAQUE, DEFENSE, STUN, GEL, SOIN, BRULURE, POISON
+    AUCUN, ATTAQUE, DEFENSE, AGILITE, STUN, GEL, SOIN, BRULURE, CONTRE, POISON
 } EffetType;
 
 // Structure pour représenter une technique spéciale
 typedef struct {
     char nom[MAX_STR];       // Nom de la technique
     char type[MAX_STR];      // Type (attaque, défense...)
-    float puissance;           // Dégâts ou valeur de l'effet
-    int portee;              // Durée de l'effet
-    int tours;               // Nombre de tours avant de reutiliser l'attaque
+    int puissance;           // Dégâts ou valeur de l'effet
+    int portee;              // Portée
+    int tours;               // Durée de l'effet
     EffetType effet;         // Type d'effet appliqué (ex : POISON, BRULURE...)
     char description[MAX_STR];
 } TechniqueSpeciale;
@@ -37,6 +46,8 @@ typedef struct {
     EffetType effets[NB_EFFETS];             // Liste des effets actifs
     int duree_effet[NB_EFFETS];              // Durées des effets
     TechniqueSpeciale techniques[MAX_TECHS]; // Techniques spéciales
-    int temps_recharge[MAX_TECHS]; // nombre de tours restants avant de réutiliser la technique
-    int prochain_tour; // nombre de tours à attendre avant de rejouer (lié à vitesse)
+    int temps_recharge[MAX_TECHS];           // nombre de tours restants avant de réutiliser la technique
+    int prochain_tour;                       // nombre de tours à attendre avant de rejouer (lié à vitesse)
 } Combattant;
+
+#endif // TYPE_H
