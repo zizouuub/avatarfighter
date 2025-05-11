df5111fbab21a41789ff19feb0f5e2dd9ed4ab79
Akani-S <akanisugir@gmail.com>
2025-05-11 17:39:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,88 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "interface.h"
+#include "type.h"
+#include "lecture.h"
+#include "effet.h"
 
 int main(){
-  srand(time(NULL)); 
+    srand(time(NULL));
+    // Lancement du menu d'accueil
+    debutJeu();
 
-if(afficheMenuPrincipal()==1){
-  int mode;
-  printf(ROUGE"Je souhaite:\n"RESET);
-  printf( "1. Jouer contre l'ordi\n" );
-  printf( "2. Jouer contre Joueur 2\n" );
-  do {
-    printf("Choisissez une option (1 ou 2):\n");
-      printf(">> ");
-      scanf("%d", &mode);
-  } while (mode != 1 && mode != 2);
-  sleep(1);
+    // Déclaration du tableau de combattants
+    Combattant tab_combattants[MAX_PERSOS];
 
-    Combattant combattants[8] = {
-      {"Zuko 🔥",     "Maître du Feu"},
-      {"Azula 🔥",    "Danseuse de Feu"},
-      {"Katara 🌊",   "Maîtresse de l'Eau"},
-      {"Sokka 🌊",    "Guerrier des Glaces"},
-      {"Toph 🌍",     "Maître de la Terre"},
-      {"Kyoshi 🌍",   "La Lame de la Terre"},
-      {"Aang 🌪",     "Maître de l'Air"},
-      {"Yangchen 🌪", "Le Souffle des Mondes"}
-    };
-
-    for (int i = 0; i < 8; i++) {
-      combattants[i].pv = rand() % 21 + 80;     // 80 à 100
-      combattants[i].attaque = rand() % 51 + 50;    // 50 à 100
-      combattants[i].defense = rand() % 31;         // 0 à 30
-
-      }
-
-        Combattant equipeJ1[3];
-        Combattant equipeJ2[3];
-        int taille_dispo = 8;
-
-  void afficherListeCombattants(Combattant* tab, int taille);
-  void selectionnerEquipe(Combattant* disponibles, int* taille_dispo, Combattant* equipe, int taille_equipe);
-  void afficherEquipe(Combattant* equipe, int taille_equipe);
-  void choisirEquipeAleatoire(Combattant* disponibles, int taille_dispo, Combattant* equipe, int taille_equipe);
-  void selectionnerEquipesJoueurs(Combattant* disponibles, int* taille_dispo, Combattant* equipeJ1, Combattant* equipeJ2, int taille_equipe);
-  
-
-      if(mode ==1){
-        printf("Vous avez choisi de jouer contre" VERT" l'ordinateur!\n"RESET );
-        sleep(2);
-        printf("\033[H\033[2J");
-
-          selectionnerEquipe(combattants, &taille_dispo, equipeJ1, 3);
-        printf("\033[H\033[2J");
-          choisirEquipeAleatoire(combattants, taille_dispo, equipeJ2, 3);
-          printf("\033[H\033[2J");
-        printf(JAUNE"Vous:\n"RESET);
-          afficherEquipe(equipeJ1, 3);
-          printf("\n");
-        printf(JAUNE"L'ordi:\n"RESET);
-          afficherEquipe(equipeJ2, 3);
-          printf(ROUGE"\nVos équipes sont prêtes à combattre!\n"RESET);
-      }else if(mode == 2){
-        printf("Vous avez choisi de jouer contre" VERT" un autre joueur!\n"RESET );
-        sleep(2);
-        printf("\033[H\033[2J");
-        selectionnerEquipesJoueurs(combattants, &taille_dispo, equipeJ1, equipeJ2, 3);
-        printf("\033[H\033[2J");
-        printf(JAUNE"Joueur 1:\n"RESET);
-          afficherEquipe(equipeJ1, 3);
-          printf("\n");
-        printf(JAUNE"Joueur 2:\n"RESET);
-          afficherEquipe(equipeJ2, 3);
-        sleep(2);
-        printf("\033[H\033[2J");
-          printf(ROUGE"\nLes équipes sont prêtes à combattre!\n"RESET);
-      }
-  
-
-      sleep(3);
-      printf("\033[H\033[2J");
-      printf("   ===  ");
-      printf(ROUGE "Le jeu va commencer🎮! " RESET);
-      printf("  ===\n");
-
+    // Chargement des combattants depuis le fichier
+    int nb_combattants = chargerCombattants("fichier.txt", tab_combattants);
+    if (nb_combattants <= 0) {
+        printf(ROUGE "Erreur: Aucun combattant chargé.\n" RESET);
+        return 1;
     }
 
-  return 0;
+    // Affichage du menu principal
+    int choix = choixPrincipal();  // Premier appel
+    if (choix == 1) {
+        // Sélection du mode de jeu
+        int mode = choixModeJeu();
+
+        // Sélection des combattants
+        printf("Vous allez maintenant choisir vos équipes de" ROUGE" 3 combattants"RESET" !\n");
+        printf("(Attention à descendre pour voir tous les combattants !)\n");
+        sleep(4);
+        printf("\033[H\033[2J");
+        
+        if (mode == 1) {
+            
+
+            // Afficher la liste des combattants une seule fois ici
+            afficheCombattants(tab_combattants, nb_combattants);
+
+            Combattant equipe_joueur[3];  // par exemple, une équipe de 3 combattants
+            selectionnerEquipe(tab_combattants, &nb_combattants, equipe_joueur, 3);
+
+            //sélection ordi 
+            Combattant equipe_ordi[3];
+            choisirEquipeAleatoire(tab_combattants, nb_combattants, equipe_ordi, 3);
+
+            //affichage équipe joueur
+            printf("\033[H\033[2J");
+            printf("   === " JAUNE"Votre équipe"RESET": ===\n");
+            afficherEquipe(equipe_joueur, 3);
+
+            //affichage équipe ordi
+            printf("\n\n");
+            printf("   === " JAUNE"Equipe de l'ordinateur"RESET": ===\n");
+            afficherEquipe(equipe_ordi, 3);
+        } else{
+
+            // Mode joueur contre joueur
+            Combattant equipe_J1[3];
+            Combattant equipe_J2[3];
+            selectionnerEquipesJoueurs(tab_combattants, &nb_combattants, equipe_J1, equipe_J2, 3);
+
+            //affichage équipe joueur 1
+            printf("\033[H\033[2J");
+            printf("   === " JAUNE"Equipe Joueur 1"RESET": ===\n");
+            afficherEquipe(equipe_J1, 3);
+
+            //affichage équipe joueur 2
+            printf("\n\n");
+            printf("   === " JAUNE"Equipe Joueur 2"RESET": ===\n");
+            afficherEquipe(equipe_J2, 3);
+        }
+
+        //affichage jeu commence
+        jeuCommence();
+    }
+
+    return 0;
 }
