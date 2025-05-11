bec95c145707a28ec1184429f2216d9a1276ec2e
linaprns <linaprrns@gmail.com>
2025-05-11 23:58:38 +0200

    Update type.h

--- type.h
+++ type.h
@@ -25,7 +25,7 @@ typedef enum {
 typedef struct {
     char nom[MAX_STR];       // Nom de la technique
     char type[MAX_STR];      // Type (attaque, défense...)
-    int puissance;           // Dégâts ou valeur de l'effet
+    float puissance;           // Dégâts ou valeur de l'effet
     int portee;              // Portée
     int tours;               // Durée de l'effet
     EffetType effet;         // Type d'effet appliqué (ex : POISON, BRULURE...)
